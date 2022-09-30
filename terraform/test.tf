986454b463d4a9ff7b59f3e0146a1025ae1a2d71
Pritish-Saha-Inviz-AI-Devops <108347210+Pritish-Saha-Inviz-AI-Devops@users.noreply.github.com>
2022-09-30 14:10:41 +0530

    Update test.tf

--- terraform/test.tf
+++ terraform/test.tf
@@ -1,1 +1,11 @@
-Hii
+provider "google"{
+    project ="inviz-gcp"
+    region="asia-south1"
+}
+
+resource "google_pubsub_topic" "fromactions" {
+    name ="fromactions"
+    project = "inviz-gcp"
+    
+  
+}
