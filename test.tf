adf6c48f72322e78ae1e3f8becd70891a1026e4f
Pritish-Saha-Inviz-AI-Devops <108347210+Pritish-Saha-Inviz-AI-Devops@users.noreply.github.com>
2022-10-07 11:24:48 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -3,6 +3,22 @@ provider "google"{
     region="asia-south1"
     credentials="temp.json"
 }
+provider "google-beta" {
+  project = "search-tf-test"
+  
+  region  = "asia-south1"
+}
+
+terraform {
+  required_version = ">= 0.13, < 2.0"
+  backend "gcs" {
+    bucket  = "search-tf-test"
+    prefix  = "terraform/pstest"
+  }
+}  
+
+
+
 
 resource "google_pubsub_topic" "fromactions" {
     name ="fromactions"
